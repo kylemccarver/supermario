0e79cc046c0df57d2ea5f16b8e0da37463eb18f9
Kyle McCarver <kmccarver8@gmail.com>
2020-03-01 15:34:18 -0700

    Update camera class to handle inputs

--- src/camera.cpp
+++ src/camera.cpp
@@ -5,8 +5,27 @@ using namespace glm;
 
 // TODO: initializer list/signature thingy
 Camera::Camera() {
+    pos = vec3(0.0, 0.0, 1.0);
+    up = vec3(0.0, 1.0, 0.0);
+    right = vec3(1.0, 0.0, 0.0);
+    direction = vec3(0.0, 0.0, -1.0);
 }
 
-mat4 Camera::look() {
-    return lookAt(pos, direction, up);
+mat4 Camera::getViewMatrix() {
+    return lookAt(pos, pos + direction, up);
+}
+
+void Camera::processInput(GLFWwindow* window, float deltaTime) {
+    if(glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+        pos += direction * speed * deltaTime;
+    if(glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+        pos-= direction * speed * deltaTime;;
+    if(glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+        pos -= right * speed * deltaTime;;
+    if(glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+        pos += right * speed * deltaTime;;
+    if(glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS)
+        pos += up * speed * deltaTime;
+    if(glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS)
+        pos -= up * speed * deltaTime;
 }
--- src/camera.h
+++ src/camera.h
@@ -1,16 +1,19 @@
 #ifndef CAMERA_H
 #define CAMERA_H
 
+#include <GLFW/glfw3.h>
 #include <glm/glm.hpp>
 
 class Camera {
     public:
         Camera();
-        glm::mat4 look();
+        glm::mat4 getViewMatrix();
+        void processInput(GLFWwindow* window, float deltaTime);
     private:
         glm::vec3 pos;
         glm::vec3 up;
         glm::vec3 direction;
         glm::vec3 right;
+        const float speed = 16.0f;
 };
 #endif
